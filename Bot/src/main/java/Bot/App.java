e6c07fb665f811f7033f9ead31815217450fa60e
Erdo <Erdo@DESKTOP-OP4IIML>
2021-03-19 16:02:55 -0400

    commit2

--- Bot/src/main/java/Bot/App.java
+++ Bot/src/main/java/Bot/App.java
@@ -10,7 +10,6 @@ public class App
 {
     public static void main( String[] args ) throws LoginException {
     	JDABuilder builder = JDABuilder.createDefault("ODA5MDgwNTIzNTAyMzg3Mjkw.YCP49A.teu4fV_d8-Dy-8ae6rsFz-MMbLk");
-		//JDA builder = JDABuilder.createDefault("ODIxNTY4ODkyNzQ4MzAwMjg4.YFFnqA.s7jw9fEVbwgd-fcWsiy4Anc_X8c").build();
 		// Disable parts of the cache
 	    //builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
 	    // Enable the bulk delete event
