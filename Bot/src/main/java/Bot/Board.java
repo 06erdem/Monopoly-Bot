d1f248b43d85e1eb84226897400e71141eb7985f
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-01 23:55:32 -0400

    Update Board.java

--- Bot/src/main/java/Bot/Board.java
+++ Bot/src/main/java/Bot/Board.java
@@ -1,10 +1,28 @@
 package Bot;
 
 public class Board {
-	Tiles tiles[] = new Tiles[41];
-	int playerList[] = new int [4];
+	Tiles[] tiles = new Tiles[41];
+	int[] playerList = new int [4];
 	
-	int
+	public Board(){
+		tiles
+	int[] getPlayerList(){
+		return playerList;
+	}
+	
+	void setPlayerList(int[] players){
+		playerList = players;
+	}
+		
+	Tiles[] getTiles(){
+		return tiles;
+	}	
+		
+	void setTiles(Tiles[] tileList){
+		tiles = tileList;
+	}
+	
+	
 	void printBoard() {
 		
 	}
