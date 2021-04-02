f3f618628f70c6ecc0cae787c973b18ceaf63498
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-02 00:24:58 -0400

    Update Tiles.java

--- Bot/src/main/java/Bot/Tiles.java
+++ Bot/src/main/java/Bot/Tiles.java
@@ -1,5 +1,23 @@
 package Bot;
 
 public class Tiles {
-
+        
+      int price; //price of tile
+      int tax; //tax applied to the tile from bank
+      boolean owned;
+      String name;
+  
+      public Tiles(int prices, int taxes, String names){
+            price = prices;
+            tax = taxes;
+            owned = false;
+            String name = names;
+      }
+  
+      void goToJail(){
+      
+      }
+  
+      
+      
 }
