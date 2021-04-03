f1fca1bae37305fda9addd8715f60ed1efdba353
Erdo <Erdo@DESKTOP-OP4IIML>
2021-04-03 01:08:01 -0400

    commit

--- Bot/src/main/java/Bot/Tiles.java
+++ Bot/src/main/java/Bot/Tiles.java
@@ -1,45 +1,9 @@
 package Bot;
 /* Parent class for all tiles of the board */
 public class Tiles {
-        
-        int price; //price of tile
-        int tax; //tax applied to the tile from bank
-        int position; //position on board
-        int rent; //rent due from the tile
-        boolean owned; //boolean to see if the tile is currently owned
-        String name; //name of the tile
-  
-        public Tiles(int prices, int taxes, String names, int index, int rents){
-                price = prices;
-                tax = taxes;
-                owned = false;
-                position = index;
-                name = names;
-                rent = rents;
-        }
-  
-        void buyProperty(){ //function to buy the property if the tile is unowned
-            owned = true;
-        }
-        void freeProperty() {
-        	owned = false;
-        }
-        int getValue() {
-        	return price;
-        }
-        int getRent(){ //function that subtracts rent from players money if the tile is owned by someone else
-                return rent;
-        }
-      
-        int getTax(){ //function that subtracts bank tax
-                return tax;
-        }
 	/* contain the coordinate of the tile on the board */
 	private int[] coordinate;
 	
 	public Tiles() {		
 	}
-  
-      
-      
 }
