ddf1709f7c2544862ddc8115efd287fa7846d946
Erdo <Erdo@DESKTOP-OP4IIML>
2021-04-03 00:52:19 -0400

    receiver

--- Bot/src/main/java/Bot/Receiver.java
+++ Bot/src/main/java/Bot/Receiver.java
@@ -1,5 +1,6 @@
 package Bot;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -16,13 +17,17 @@ public class Receiver extends ListenerAdapter {
 		if(e.getMessage().getContentRaw().toLowerCase().contains("printboard")) {
 			e.getChannel().sendMessage(board_temp.printBoard()).queue();
 		}
-		/*for testing purpose, should be deleted afterwards.*/
-		Board board_temp = new Board();
-		if(e.getMessage().getContentRaw().toLowerCase().contains("printboard")) {
-			e.getChannel().sendMessage(board_temp.printBoard()).queue();
-		}
-		if(e.getMessage().getContentRaw().toLowerCase().contains("!delete")) {
+
+		if(content.contains("!delete")) {
 			e.getChannel().purgeMessages(e.getChannel().getHistory().retrievePast(50).complete());
+		}
+		if(content.equals("!play")) {
+			EmbedBuilder embed = new EmbedBuilder();
+			embed.setTitle("Welcome to Discord Monopoly!");
+			embed.setDescription("Select your player emoji.\n1 :pickup_truck:    2 :race_car:    3 :bus:    4 :motorcycle:");
+			embed.setFooter("Type 1, 2, 3 or 4.\n2-4 Players required\nPlayers cannot choose the same emoji. Type '!start' when ready.");
+			e.getChannel().sendMessage(embed.build()).queue();
+			//e.getMessage().addReaction("U+2705").queue();
 		}
 	}
 	
