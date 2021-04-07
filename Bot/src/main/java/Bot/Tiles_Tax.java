01a00d2cd14a366a03254baf9c32fd4ed7e27965
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-07 10:11:36 -0400

    Update Tiles_Tax.java

--- Bot/src/main/java/Bot/Tiles_Tax.java
+++ Bot/src/main/java/Bot/Tiles_Tax.java
@@ -1,18 +1,24 @@
 package Bot;
 
 public class Tiles_Tax extends Tiles {
-	int tax;
-	public Tiles_Tax(int tax) {
-		name = "Tax";
-		type = 5;
-		this.tax = tax;
-		emoji = ":money_with_wings:";
-	}
-	public Tiles_Tax(int tax, String emoji) {
-		name = "Tax";
-		type = 5;
-		this.emoji = emoji;
-		this.tax = tax;
-	}
-
+ int tax;
+ public Tiles_Tax(int tax) {
+  name = "Tax";
+  type = 5;
+  this.tax = tax;
+  emoji = ":money_with_wings:";
+ }
+ public Tiles_Tax(int tax, String emoji) {
+  name = "Tax";
+  type = 5;
+  this.emoji = emoji;
+  this.tax = tax;
+ }
+ int getTax(){
+   return tax;
+ }
+ 
+ void setTax(int t){
+   tax = t;
+}
 }
