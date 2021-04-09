e194cc7d5a144c506f841c05e4001433170b95e4
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-08 23:03:41 -0400

    Update Tiles_Chance.java

--- Bot/src/main/java/Bot/Tiles_Chance.java
+++ Bot/src/main/java/Bot/Tiles_Chance.java
@@ -1,15 +1,24 @@
 package Bot;
+import java.lang.*;
 
 public class Tiles_Chance extends Tiles { 
-	
-	public Tiles_Chance() {
-		name = "Chance!";
-		type = 4;
-		emoji = ":question:";
-	}
-	public Tiles_Chance(String emoji) {
-		name = "Chance!";
-		type = 4;
-		this.emoji = emoji;
-	}
+ int min = -50;
+ int max = 50;
+ int random;
+ public Tiles_Chance() {
+  name = "Chance!";
+  type = 4;
+  emoji = ":question:";
+  random = (int)Math.floor(Math.random()*(max-min+1)+min);
+ }
+ public Tiles_Chance(String emoji) {
+  name = "Chance!";
+  type = 4;
+  this.emoji = emoji;
+  random = (int)Math.floor(Math.random()*(max-min+1)+min);
+ }
+ 
+ public int getRandom(){
+   return random;
+ }
 }
