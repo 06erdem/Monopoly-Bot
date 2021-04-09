34062fd2951349a2862b15814d2466e45107e78e
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-09 00:59:34 -0400

    Update Tiles_Start.java

--- Bot/src/main/java/Bot/Tiles_Start.java
+++ Bot/src/main/java/Bot/Tiles_Start.java
@@ -1,16 +1,30 @@
 package Bot;
 
 public class Tiles_Start extends Tiles {
+  boolean hasOwner = false;
+  int rent = 0;
+  int random = 0;
+  public Tiles_Start() {
+    // TODO Auto-generated constructor stub
+    type = 0;
+    name = "Collect $200 as you pass!";
+    emoji = ":green_circle:";
+  }
 
-	public Tiles_Start() {
-		// TODO Auto-generated constructor stub
-		type = 0;
-		name = "Collect $200 as you pass!";
-		emoji = ":green_circle:";
-	}
-
-	int getReward() {
-		return 200;
-	}
-	public String toString() { return ":checkered_flag:"; }
+  public boolean hasOwner(){
+    return hasOwner;
+  }
+ 
+  public int getRent(){
+    return rent;
+  }
+ 
+  public int getRandom(){
+    return random;
+  }
+ 
+  int getReward() {
+    return 200;
+  }
+  public String toString() { return ":checkered_flag:"; }
 }
