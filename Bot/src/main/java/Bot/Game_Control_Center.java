808b6af7b575fb9d74efc2229539f4959f3f3a90
yhash123 <78493823+yhash123@users.noreply.github.com>
2021-04-16 00:01:48 -0400

    Update Game_Control_Center.java

--- Bot/src/main/java/Bot/Game_Control_Center.java
+++ Bot/src/main/java/Bot/Game_Control_Center.java
@@ -139,9 +139,12 @@ public class Game_Control_Center {
 					Player currentPlayer = board.playerList[board.getCurrPlayer()];
 					int initialPosition = currentPlayer.getPosition();
 					String diceOutput = board.rollDice();
-     				String[] dice = diceOutput.split(" ");
-     				int dice1 = Integer.parseInt(dice[0]);
-     				int dice2 = Integer.parseInt(dice[1]);
+     					String[] dice = diceOutput.split(" ");
+     					int dice1 = Integer.parseInt(dice[0]);
+     					int dice2 = Integer.parseInt(dice[1]);
+					if(currentPlayer.getPosition() + dice1 + dice2 == 30){
+       					   sendGenericEmbed("Sent to Jail!", "You have been sent back to jail! Your turn is up.", null);
+      					}	
 					int moveState = board.movePosition(dice1, dice2, board.getCurrPlayer()); //TODO:Add dice call & dice values to movePosition call
 					if((initialPosition > currentPlayer.getPosition()) && (currentPlayer.getInJail() == false)){
 						//Print passing GO message
