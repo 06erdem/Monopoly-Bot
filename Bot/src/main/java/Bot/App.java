4a8c7a7253cfd086e114364b64e8fbd07506ae68
erdem1905gs <76748586+erdem1905gs@users.noreply.github.com>
2021-04-28 21:25:22 -0400

    Update App.java

--- Bot/src/main/java/Bot/App.java
+++ Bot/src/main/java/Bot/App.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.entities.Activity;
 public class App
 {
     public static void main( String[] args ) throws LoginException {
-       JDABuilder builder = JDABuilder.createDefault("ODIyNTcyNTc1OTEzMjc5NTU0.YFUOaA.PnZkhioRqz5V_Q_cUv7mH6Gbs5U");
+       JDABuilder builder = JDABuilder.createDefault("Enter key here");
 	   builder.setActivity(Activity.watching("CS 321 Vids"));
 	   Receiver receiver = new Receiver();
 	   builder.addEventListeners(receiver);
